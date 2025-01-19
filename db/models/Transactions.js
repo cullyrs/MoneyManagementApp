1d6d45fc3bf6bcf51db9074bef0c766afbf0f579
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-01-19 14:37:42 -0600

    Update Transactions.js

--- db/models/Transactions.js
+++ db/models/Transactions.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const transactionSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  transaction: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   amount: { type: Number, required: true },
   type: { type: String, enum: ['income', 'expense'], required: true },
   date: { type: Date, default: Date.now },
-  categoryId: { type: Schema.Types.ObjectId, ref: 'Category' },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
   notes: { type: String }
 }, { timestamps: true });
 
