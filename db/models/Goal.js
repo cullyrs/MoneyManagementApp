80078b14edf5cf705edabda28eb5f7ebe7360206
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-01-19 14:39:16 -0600

    Update Goal.js

--- db/models/Goal.js
+++ db/models/Goal.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const goalSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  goal: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   goalName: { type: String, required: true },
   targetAmount: { type: Number, required: true },
   savedAmount: { type: Number, default: 0 },
   savedToDate: { type: Date, default: Date.now },
-  categoryId: { type: Schema.Types.ObjectId, ref: 'Category' }
+  category: { type: Schema.Types.ObjectId, ref: 'Category' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Goal', goalSchema);
