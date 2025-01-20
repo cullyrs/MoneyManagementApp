d4d5edef59d7cab169203f333b9eb2e31b657d89
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-20 09:32:15 -0500

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,27 +1,38 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cred = require('./api.json');
-const uri = `mongodb+srv://${cred.username}:${cred.password}@expensemanager1.3yfoo.mongodb.net/?retryWrites=true&w=majority&appName=ExpenseManager1`;
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { username, password } from './api.json';
+const uri = `mongodb+srv://${username}:${password}@expensemanager1.3yfoo.mongodb.net/?retryWrites=true&w=majority&appName=ExpenseManager1`;
+let client;
+let db;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+async function connectToDB() {
+  if (!client) {
+    client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
   }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
+  
+  // Connect the client to the server 
+  if (!client.topology || !client.topology.isConnected()) {
     await client.connect();
-    // Send a ping to confirm a successful connection
+    console.log("Connected to MongoDB!");
+    
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
+    db = client.db('Accounts');
   }
+  return db; 
 }
-run().catch(console.dir);
+
+function getDB() {
+  if (!db) throw new Error("DB not connected yet. Call connectToDB() first.");
+  return db;
+}
+
+export default { connectToDB, getDB };
