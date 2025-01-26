07f5004d09b23b7f4313c97c25b43d14d3894184
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-01-26 16:47:26 -0600

    Update Transactions.js

--- db/models/Transactions.js
+++ db/models/Transactions.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+//Must install mongoose int 32 plugin "npm install mongoose-int32"
+const Int32 = require ('mongoose-int32').loadType(mongoose);
+const { Schema } = (mongoose);
 
 const transactionSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  amount: { type: Number, required: true },
+  amount: { type: Int32, required: true },
   type: { type: String, enum: ['income', 'expense'], required: true },
   date: { type: Date, default: Date.now },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
