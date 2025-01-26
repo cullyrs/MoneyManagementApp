d411cd3c8b3ec236863eee2af7e9bfec9ccb880a
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-01-26 16:37:20 -0600

    Update Budget.js

--- db/models/Budget.js
+++ db/models/Budget.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+//Must install mongoose int 32 plugin "npm install mongoose-int32"
+const Int32 = require ('mongoose-int32).loadType(mongoose);
 const { Schema } = mongoose;
 
 const BudgetSchema = new Schema({
   budgetId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   budgetName: { type: String, required: true },
-  amount: { type: Number, required: true },
+  amount: { type: Int32, required: true },
   duration: {
     type: Date,
     default: () => new Date(Date.now() + 7 * 24 * 3600 * 1000), // one week from now
