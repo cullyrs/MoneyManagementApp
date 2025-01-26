5ca15272db6e379ec904af923abc72ede10f5645
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-01-26 16:51:18 -0600

    Update Category.js

--- db/models/Category.js
+++ db/models/Category.js
@@ -23,31 +23,31 @@ module.exports = model('Category', categorySchema);
 await Category.create({
   id: 0,
   name: "All",
-  schema: 1.1
+  version: 1
 });
 
 await Category.create({
   id: 1,
   name: "Housing",
-  schema: 1.1
+  version: 1
 });
 
 await Category.create({
   id: 2,
   name: "Transportation",
-  schema: 1.1
+  version: 1
 });
 
 await Category.create({
   id: 3,
   name: "Vehicle",
-  schema: 1.1
+  version: 1
 });
 
 await Category.create({
   id: 4,
   name: "Life & Entertainment",
-  schema: 1.1
+  version: 1
 });
 
 
