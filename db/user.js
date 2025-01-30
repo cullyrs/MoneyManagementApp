e88dfe17a4aae9388f8a67a7630ffa30f22fb242
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-30 09:14:17 -0500

    Update user.js
    standardized connection
    updated function in module
       * createUser => addUser
       * findUser
    added functions to module
       * updateTotalAmount
       * updateUsername
       * updateEmail
       * addTransaction

--- db/user.js
+++ db/user.js
@@ -1,41 +1,89 @@
 import mongoose from "mongoose";
 import User from "./models/User.js";
+import Transaction from "./models/Transaction.js";
+import Category from "./models/Category.js";
 import hashed,{compareEntry} from "../utils/helper.js";
-import cin from "../api.json" with {"type" : "json"};
-const uri = `mongodb+srv://${cin.username}:${cin.password}@expensemanager1.3yfoo.mongodb.net/Accounts?retryWrites=true&w=majority&appName=ExpenseManager1`;
-
-mongoose.connect(uri);
+import connectDB from "./dbconnect.js"; connectDB();
 
 // Create a new user
-const  addUser = async (uname, entry, email_,amount)=>{
+const  addUser = async (name, entry, email,amount)=>{
     const user = await User.create({
-         userName: uname,
+         userName: name,
          password : await hashed(entry),
-         email : email_,
+         email : email,
          totalAmount : parseFloat(amount)
     });
     return user;
 }
-const updateEmail = async (oldEmail, newEmail) =>{
-    const found = await User.exists({email: oldEmail});
-    if(found){
-        const user = await User.where("_id").equals(found._id);
-        user[0].email = newEmail;
+const updateTotalAmount = async(userID, newAmount)=>{
+    const user = await User.where("_id").equals(userID);
+    if(user[0]){
+        user[0].totalAmount = parseFloat(newAmount);
         await user[0].save();
+        return user[0];
     }
     else{
-        console.log("This user does not exist.");
+        return null;
+    }
+
+}
+const updateUsername = async(userID, newName)=>{
+    const user = await User.where("_id").equals(userID);
+    if(user[0]){
+        user[0].userName = newName;
+        await user[0].save();
+        return user[0];
+    }
+    else{
+        return null;
+    }
+
+}
+const updateEmail = async (oldEmail, newEmail) =>{
+    const user = await User.where("email").equals(oldEmail);
+    if(user[0]){
+        user[0].email = newEmail;
+        await user[0].save();
+        return user[0];
+    }
+    else{
+        return null;
     }
 }
 const findUser = async(email) =>{
-    const find = await User.where("email").equals(email);
-    return find;
+    const user = await User.where("email").equals(email);
+    if(user[0]){
+        return user[0];
+    }
+    return null;
 }
 const loginUser = async(email_, entry) =>{
-    const login = await User.where("email").equals(email_);
-    const valid = await compareEntry(entry,login[0].password);
-    return valid;
+    const user = await User.where("email").equals(email_);
+    if(user[0] && await compareEntry(entry,user[0].password)){
+        return user[0];
+    }
+    return null;
 }
-export{ addUser as default, findUser, loginUser, updateEmail};
+const addTransaction = async(userID, amount,  notes ="", category = 0,type = 'expense') =>{
+    const user = await User.where("_id").equals(userID);
+    if(user[0]){
+        const transaction = await Transaction.create({
+            userID: user[0]._id,
+            amount : parseFloat(amount),
+            type : type,
+            category : category,
+            notes : notes
+       });       
+        user[0].transactionList.push(transaction._id);
+        await user[0].save();
+        return user[0];
+    }
+    else{
+        return null;
+    }
+}
+export{ addUser as default, findUser, loginUser, updateEmail,
+        updateTotalAmount, updateUsername, addTransaction
+};
 
 
