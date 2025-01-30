de3cce98f2a07881775d40d2e4024d0d9e9c3200
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-30 11:32:04 -0500

    Update Category.js

--- db/models/Category.js
+++ db/models/Category.js
@@ -1,55 +1,14 @@
-const mongoose = require('mongoose');
-//Must install mongoose int 32 plugin "npm install mongoose-int32"
-const Int32 = require ('mongoose-int32').loadType(mongoose);
-const { Schema } = (mongoose);
-                       
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 const categorySchema = new Schema({
-  id: {type: Int32, required: true, unique: true},
-  version: {type: Int32, default: 1},
-  userId: {type: Schema.Types.ObjectId, ref: 'User'},
+  id: {type: Number, required: true, unique: true},
+  __v: {type: Number, default: 0},
   name: {type: String, required: true, trim: true, minlength: 1, maxlength: 50},
-  type: {type: String, enum: ['income', 'expense', 'both'], default: 'expense'},
   icon: String,
   color: String,
   description: String,
   isActive: {type: Boolean,default: true},
   parentCategory: {type: Schema.Types.ObjectId, ref: 'Category'}
-}, {collection: 'Category', timestamps: true });
-
-categorySchema.index({ userId: 1, name: 1 }, { unique: true });
-module.exports = model('Category', categorySchema);
-
-//General Categories
-await Category.create({
-  id: 0,
-  name: "All",
-  version: 1
-});
-
-await Category.create({
-  id: 1,
-  name: "Housing",
-  version: 1
-});
-
-await Category.create({
-  id: 2,
-  name: "Transportation",
-  version: 1
-});
-
-await Category.create({
-  id: 3,
-  name: "Vehicle",
-  version: 1
-});
-
-await Category.create({
-  id: 4,
-  name: "Life & Entertainment",
-  version: 1
-});
-
-
-
-
+}, { collection : 'Category',timestamps: true });
+const Category = model('Category', categorySchema);
+export default Category;
