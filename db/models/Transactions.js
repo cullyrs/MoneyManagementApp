262006b35f8f4abd846f556ee607927d650e0a6b
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-31 10:04:49 -0500

    Update Transactions.js

--- db/models/Transactions.js
+++ db/models/Transactions.js
@@ -7,17 +7,17 @@
  * Project : Expense Tracker Capstone Project
  * Description : The purpose of the Transaction.js module is to build
  * and export the schema for the Transaction collection in the 
- * Expense Tracker Accounts database.
+ * Expense Tracker Accounts database..
  */
 import mongoose from "mongoose";
-const { Schema, model } = mongoose;
+const { Schema, SchemaTypes, model } = mongoose;
 
 const transactionSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  amount: { type: Schema.Types.Double, required: true },
-  type: { type: Number, required: true },
-  date: { type: Schema.Types.Date, default: Date.now },
-  category: { id: Number, name : String},
+  userId: { type: SchemaTypes.ObjectId, ref: 'User', required: true },
+  amount: { type: SchemaTypes.Double, required: true },
+  type: { type: SchemaTypes.Int32, required: true },
+  date: { type: SchemaTypes.Date, default: Date.now },
+  category: { id: SchemaTypes.Int32, name : String},
   description: { type: String },
   version: {type: Number, default : 1}
 }, { collection : 'Transaction', timestamps: true });
