c39c987b5ff82b2df0eada965700593a953ae3fe
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-31 07:28:05 -0500

    Update Transactions.js
    Added documentation

--- db/models/Transactions.js
+++ db/models/Transactions.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
-//Must install mongoose int 32 plugin "npm install mongoose-int32"
-const Int32 = require ('mongoose-int32').loadType(mongoose);
-const { Schema } = (mongoose);
+/**
+ * Name : Arewa (Morountudun) Ojelade
+ * Contributors : Steven Mounie, and Cully Stearns
+ * Date : 1/31/2025
+ * File Name: Transaction.js
+ * Course : CMSC 495 Capstone in Computer Science
+ * Project : Expense Tracker Capstone Project
+ * Description : The purpose of the Transaction.js module is to build
+ * and export the schema for the Transaction collection in the 
+ * Expense Tracker Accounts database..
+ */
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
 
 const transactionSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  amount: { type: Int32, required: true },
-  type: { type: String, enum: ['income', 'expense'], required: true },
-  date: { type: Date, default: Date.now },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
-  notes: { type: String }
-}, {collection: 'Transaction', timestamps: true });
+  amount: { type: Schema.Types.Double, required: true },
+  type: { type: Number, required: true },
+  date: { type: Schema.Types.Date, default: Date.now },
+  category: { id: Number, name : String},
+  description: { type: String },
+  version: {type: Number, default : 1}
+}, { collection : 'Transaction', timestamps: true });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+const Transaction = model('Transaction', transactionSchema);
+export default Transaction;
