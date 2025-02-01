16e5d8eeb11c947c625e959a640609acb9fd615f
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-01-31 19:58:24 -0500

    Update Goal.js

--- db/models/Goal.js
+++ db/models/Goal.js
@@ -1,15 +1,28 @@
-const mongoose = require('mongoose');
-//Must install mongoose int 32 plugin "npm install mongoose-int32"
-const Int32 = require ('mongoose-int32').loadType(mongoose);
-const { Schema } = (mongoose);
+/**
+ * Name : Arewa (Morountudun) Ojelade
+ * Contributors : Steven Mounie, and Cully Stearns
+ * Date : 1/31/2025
+ * File Name: Budget.js
+ * Course : CMSC 495 Capstone in Computer Science
+ * Project : Expense Tracker Capstone Project
+ * Description : The purpose of the Goal.js module is to build
+ * and export the schema for the Goal collection in the 
+ * Expense Tracker Accounts database.
+ */
+
+import mongoose from "mongoose";
+const { Schema, SchemaTypes, model } = mongoose;
 
 const goalSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  goalName: { type: String, required: true },
-  targetAmount: { type: Int32, required: true },
-  savedAmount: { type: Int32, default: 0 },
-  savedToDate: { type: Date, default: Date.now },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' }
+  userId: { type: SchemaTypes.ObjectId, ref: 'User', required: true },
+  name: { type: String, required: true },
+  targetAmount: { type: SchemaTypes.Double, required: true },
+  savedAmount: { type: SchemaTypes.Double, default: 0 },
+  savedToDate: { type: Date, 
+    default: ()=> new Date(Date.now) + 30 * 24 * 3600 * 1000, // default month duration
+  }, 
+  categoryID: { type: SchemaTypes.Int32, default : 0},
+  version : {type : SchemaTypes.Int32, default : 1}
 }, { collection : 'Goal', timestamps: true });
 
-module.exports = mongoose.model('Goal', goalSchema);
+module.exports = model('Goal', goalSchema);
