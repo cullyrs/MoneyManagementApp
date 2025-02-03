f25f7bb1d695af7b09ad2e935e31fc1d63c68094
Arewa-Iyi <125617794+Arewa-Iyi@users.noreply.github.com>
2025-02-03 09:02:14 -0500

    Update user.js
    Added return of an array of user object, transactionList, budgetList, and goalList to loginUser function.

--- db/user.js
+++ db/user.js
@@ -5,15 +5,15 @@
  * Course : CMSC 495 Capstone in Computer Science
  * Project : Expense Tracker Capstone Project
  * Description : The purpose of the user.js module is to provide 
- * compact functions to interact with the User collection of
- * the Expence Tracker Accounts database.
+ * compact functions to Interact with the User collection of
+ * the Expense Tracker Accounts database.
  */
-import mongoose from "mongoose";
+import connectDB from "./dbconnect.js"; connectDB();
 import User from "./models/User.js";
 import Transaction from "./models/Transaction.js";
-import Category from "./models/Category.js";
+import Budget from "./models/Budget.js";
+import Goal from "./models/Goal.js";
 import hashed,{compareEntry} from "../utils/helper.js";
-import connectDB from "./dbconnect.js"; connectDB();
 
 /**
  * Function to add a user to the User collection of the 
@@ -21,77 +21,21 @@ import connectDB from "./dbconnect.js"; connectDB();
  * @param {String} name - The name of the user. 
  * @param {String} entry - The string provided to access user account.
  * @param {String} email - The email of the user.
- * @param {Number} amount - The total amount of funds currently available.
+ * @param {Double} amount - The total amount of funds currently available.
  * @returns {Object} The  created instance of the user object.
+  * Returns null if :
+ *      1. Invalid name/entry/email is provided. (Empty String)
+ *      2. Invalid amount is provided (Positive values only)
  */
 const  addUser = async (name, entry, email,amount)=> {
-    const user = await User.create({
-         userName: name,
-         password : await hashed(entry),
-         email : email,
-         totalAmount : parseFloat(amount)
-    });
-    return user;
-}
-/**
- * Function to update the toatal amount in the User collection of the 
- * Expense Tracer Accounts database.
- * @param {String} userID - The unique id of the User instance. 
- * @param {Number} newAmount - The updated total amount currently available.
- * @returns {Object} The updated instance of the user object. 
- * Returns null if invalid userID is provided.
- */
-const updateTotalAmount = async(userID, newAmount)=>{
-    
-    const user = await User.findById(userID).exec();
-    if(user){
-        user.totalAmount = parseFloat(newAmount);
-        await user.save();
+    if(name && entry && email && amount > 0){
+        const user = await User.create({
+            userName: name,
+            password : await hashed(entry),
+            email : email,
+            totalAmount : parseFloat(amount)
+        });
         return user;
-    }
-    return null;
-
-}
-/**
- * Function to update the user's name in the User collection of the 
- * Expense Tracer Accounts database.
- * @param {String} userID - The unique id of the User instance. 
- * @param {String} newName - The updated name.
- * @returns {Object} The updated instance of the user object. 
- * Returns null if invalid userID is provided.
- */
-const updateUsername = async(userID, newName)=> {
-    const user = await User.findById(userID).exec();
-    if(user){
-        user.userName = newName;
-        await user.save();
-        return user;
-    }
-    return null;
-
-}
-/**
- * Function to update the user's email in the User collection of the 
- * Expense Tracer Accounts database.
- * @param {String} userID - The unique id of the User instance. 
- * @param {String} oldEmail - The current email of the User instance.
- * @param {String} newEmail - The updated email of the User instance.
- * @returns {Object} The updated instance of the user object. 
- * Returns null if :
- *      1. Invalid userID is provided.
- *      2. Unassociated email is provided.
- */
-const updateEmail = async (userID, oldEmail, newEmail) => {    
-    const user = await User.findOne({_id : userID}).where("email").equals(oldEmail);
-    console.log(user);
-    if(user){
-        try{
-            user.email = newEmail;
-            await user.save();
-            return user;
-        }catch(error){
-            return null;
-        }
     }
     return null;
 }
@@ -102,8 +46,24 @@ const updateEmail = async (userID, oldEmail, newEmail) => {
  * @returns {Object} The instance of the associated user object.
  * Returns null if User collection is unassociated with email provided.
  */
-const findUser = async(email) => {
+const getUser = async(email) => {
+    email = email.toLowerCase();
     const user = await User.findOne({email : email});
+    if(user){
+        return user;
+    }
+    return null;
+}
+
+/**
+ * Function to find a user instance with unique _id in the User collection of the 
+ * Expense Tracer Accounts database.
+ * @param {String} userID - The unique id of the User instance. 
+ * @returns {Object} The instance of the associated user object.
+ * Returns null if User collection is unassociated with email provided.
+ */
+const findUser = async(userID) => {
+    const user = await User.findOne({_id : userID});
     if(user){
         return user;
     }
@@ -114,32 +74,112 @@ const findUser = async(email) => {
  * Expense Tracer Accounts database.
  * @param {String} email - The current email of the User instance.
  * @param {String} entry - The string provided to access user account.
- * @returns {Object} The instance of the associated user object.
+ * @returns {Object} An array in the following indices:
+ *                  0. The instance of the associated user object.
+ *                  1. The transaction objects associated with the user.
+ *                  2. The budget objects associated with the user.
+ *                  3. The goal objects associated with the user.
  * Returns null if:
- *      1.  User collection is unassociated with email provided.
- *      2. Invalid entry is provided.
+ *      1. User collection is unassociated with email provided.
+ *      2. Invalid entry is provided. (Current entry mismatch)
  */
 const loginUser = async(email, entry) => {
+    email = email.toLowerCase();
     const user = await User.findOne({email : email});
-    if(user && await compareEntry(entry,user.password)){
+    if(user && await compareEntry(entry, user.password)){        
+        const transactions = await Transaction.where("userID").equals(user._id);
+        const budgets = await Budget.where("userID").equals(user._id);
+        const goals = await Goal.where("userID").equals(user._id);
+        return [user,transactions,budgets, goals];
+    }
+    return null;
+}
+/**
+ * Function to update the toatal amount in the User collection of the 
+ * Expense Tracer Accounts database.
+ * @param {String} userID - The unique id of the User instance. 
+ * @param {Double} newAmount - The updated total amount currently available.
+ * @returns {Object} The updated instance of the user object. 
+ * Returns null if :
+ *      1. Invalid userID is provided.
+ *      2. Invalid amount is provided (Positive values only)
+ */
+const updateTotalAmount = async(userID, newAmount)=>{
+    
+    const user = await User.findById(userID).exec();
+    if(user && newAmount > 0){
+        user.totalAmount = parseFloat(newAmount);
+        await user.save();
         return user;
     }
     return null;
 }
+
+/**
+ * Function to update the user's name in the User collection of the 
+ * Expense Tracer Accounts database.
+ * @param {String} userID - The unique id of the User instance. 
+ * @param {String} newName - The updated name.
+ * @returns {Object} The updated instance of the user object. 
+  * Returns null if :
+ *      1. Invalid userID is provided.
+ *      2. Invalid newName is provided. (Empty String)
+ */
+const updateUserName = async(userID, newName)=> {
+    const user = await User.findById(userID).exec();
+    if(user && newName){
+        user.userName = newName;
+        await user.save();
+        return user;
+    }
+    return null;
+
+}
+
+/**
+ * Function to update the user's email in the User collection of the 
+ * Expense Tracer Accounts database.
+ * @param {String} userID - The unique id of the User instance. 
+ * @param {String} oldEmail - The current email of the User instance.
+ * @param {String} newEmail - The updated email of the User instance.
+ * @returns {Object} The updated instance of the user object. 
+ * Returns null if :
+ *      1. Invalid userID is provided.
+ *      2. Invalid newEmail is provided. (Empty String)
+ *      3. Unassociated email is provided.
+ */
+const updateEmail = async (userID, oldEmail, newEmail) => { 
+    oldEmail = oldEmail.toLowerCase();
+    newEmail = newEmail.toLowerCase();   
+    const user = await User.findOne({_id : userID}).where("email").equals(oldEmail);
+    console.log(user);
+    if(user && newEmail){
+        try{
+            user.email = newEmail;
+            await user.save();
+            return user;
+        }catch(error){
+            return null;
+        }
+    }
+    return null;
+}
+
 /**
  * Function to update password of a user instance in the User collection of the 
  * Expense Tracer Accounts database.
  * @param {String} userID - The current unique id of the User instance.
  * @param {String} oldEntry - The current string to access user account.
  * @param {String} newEntry - The updated string provided to access user account.
- * @returns {Object} The updated instance of the associated user object.
+ * @returns {Object} The pudated instance of the associated user object.
  * Returns null if:
  *      1.  User collection is unassociated with id provided.
- *      2.  Invalid entry is provided.
+ *      2.  Invalid oldEntry is provided. (Current entry mismatch)
+ *      3. Invalid newEntry is provided. (Empty String)
  */
 const updatePassword = async(userID, oldEntry, newEntry) => {
     const user = await User.findOne({_id : userID});
-    if(await loginUser(user.email, oldEntry)){
+    if(await loginUser(user.email, oldEntry) && newEntry){
         user.set('password', await hashed(newEntry));
         await user.save();
         return user;
@@ -147,8 +187,8 @@ const updatePassword = async(userID, oldEntry, newEntry) => {
     return null;
 }
 
-export{ addUser as default, findUser, loginUser, updateEmail,
-        updatePassword, updateTotalAmount, updateUsername
+export {addUser, findUser, getUser, getUserID, loginUser, updateEmail,
+        updatePassword, updateTotalAmount, updateUserName
 };
 
 
