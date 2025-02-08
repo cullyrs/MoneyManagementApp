88f2a40792028dd2fcb6ee98a14080a3f49a31f8
cullyrs <49454803+cullyrs@users.noreply.github.com>
2025-02-08 11:13:27 -0600

    Update ipcHandlers.js

--- ipcHandlers/ipcHandlers.js
+++ ipcHandlers/ipcHandlers.js
@@ -1,5 +1,6 @@
 /**
- * Name : Arewa (Morountudun) Ojelade
+ * Name : Cully Stearns
+ * Contributors : Arewa 
  * Date : 1/31/2025
  * File Name : ipcHandlers.js
  * Course : CMSC 495 Capstone in Computer Science
